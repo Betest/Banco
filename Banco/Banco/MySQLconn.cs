24178efc3e816f61e4f955bb2b937004a7925cb8
Yohan Tobon <yohan.tobon@hotmail.com>
2019-09-04 23:35:07 -0500

    Login

---
+++ Banco/Banco/MySQLconn.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace Banco
+{
+    public class MySQLconn
+    {
+        private string user;
+        private string password;
+
+        public MySQLconn(string user, string password)
+        {
+            this.user = user;
+            this.password = password;
+        }
+
+        MySqlConnectionStringBuilder Builder = new MySqlConnectionStringBuilder();
+
+        //public bool TryConnection(out string Error)
+        //{
+        //    try
+        //    {
+        //        Builder.Server = "files.000webhost.com";
+        //        Builder.UserID = "betestsas";
+        //        Builder.Password = "shadow893";
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        throw;
+        //    }
+            
+        //}
+
+    }
+}
